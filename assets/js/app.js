521889afe59ca6de69fd1bfebf47121a69d91f30
yousefarab1 <yousefgamal1412005@gmail.com>
2026-05-03 00:05:34 +0300

    second day of work

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,36 @@ fetch("../components/footer.html")
 .then(html => {
 document.getElementById("footer").innerHTML = html;
 });
+
+
+const num = document.getElementsByClassName("number")[0]
+const price = Number(document.getElementsByClassName('price')[0].innerText)
+const total_price = document.getElementsByClassName('total-price')[0]
+const total_price1 = document.getElementsByClassName('total-price')[1]
+var count = 0
+
+function increase(e) {
+    count++
+    update()
+}
+
+function decrease(e) {
+    if (count > 0) {
+        count--
+        update()
+    }
+}
+
+function update() {
+    num.innerText = count
+    const total = price * count
+    total_price.innerText = total
+    total_price1.innerText = total
+}
+
+
+
+
+
+
+
